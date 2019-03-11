dfd218c5541d4954db4286ad319ce2a9bbc6744d
Marcin Dziewulski <hello@mobily.pl>
2019-03-11 23:12:33 +0100

    Update `getWithDefault` (Option)

--- src/option/getWithDefault.ts
+++ src/option/getWithDefault.ts
@@ -1,7 +1,7 @@
-import { None } from './None'
+import { isNone } from './isNone'
 
 import { Option } from '../internal/types'
 
 export const getWithDefault = <T>(defaultValue: NonNullable<T>) => (
   option: Option<T>,
-): T => (option === None ? defaultValue : (option as NonNullable<T>))
+): T => (isNone(option) ? defaultValue : (option as NonNullable<T>))
