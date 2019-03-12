8c36f8226c07c57e640726bcf9ac3452df4f9cb2
Marcin Dziewulski <hello@mobily.pl>
2019-03-12 21:18:01 +0100

    Add `ExtractValue<T>` helper

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -8,3 +8,4 @@ export type None = Fn<any> & { _: 'None' }
 
 export type Option<T> = None | Some<T>
 export type UnaryFunction<T, R> = (source: T) => R
+export type ExtractValue<T> = T extends NonNullable<infer R> ? R : never
