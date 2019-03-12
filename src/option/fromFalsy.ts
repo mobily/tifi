213a6a4c9248720ea47695aa7842cad7dc5b8d12
Marcin Dziewulski <hello@mobily.pl>
2019-03-12 22:59:02 +0100

    Add ts overloads to `fromFalsy` function

--- src/option/fromFalsy.ts
+++ src/option/fromFalsy.ts
@@ -1,7 +1,10 @@
 import { Some } from './Some'
 import { None } from './None'
 
-import { Option } from '../internal/types'
+import { Option, None as OpNone, Some as OpSome } from '../internal/types'
 
-export const fromFalsy = <T>(value: T): Option<T> =>
-  Boolean(value) ? Some(value) : None
+export function fromFalsy(value: 0 | '' | false | null | undefined): OpNone
+export function fromFalsy<T>(value: T): OpSome<T>
+export function fromFalsy<T>(value: T): Option<T> {
+  return Boolean(value) ? Some(value) : None
+}
