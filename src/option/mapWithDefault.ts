7102dc74600d44a1cf6b642f784764185ea78f87
Marcin Dziewulski <hello@mobily.pl>
2019-03-12 10:21:25 +0100

    Tweaks

--- src/option/mapWithDefault.ts
+++ src/option/mapWithDefault.ts
@@ -6,6 +6,5 @@ import { Option } from '../internal/types'
 export const mapWithDefault = <T, R>(
   defaultValue: NonNullable<R>,
   fn: (arg0: NonNullable<T>) => NonNullable<R>,
-) => (option: Option<T>): Option<R> => {
-  return Some(isNone(option) ? defaultValue : fn(option as NonNullable<T>))
-}
+) => (option: Option<T>): Option<R> =>
+  Some(isNone(option) ? defaultValue : fn(option as NonNullable<T>))
