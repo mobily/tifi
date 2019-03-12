b9f4710f9cd4452a38bcc12da173d58fe5fcde6c
Marcin Dziewulski <hello@mobily.pl>
2019-03-12 22:47:44 +0100

    Update `None` value

--- src/option/None.ts
+++ src/option/None.ts
@@ -2,6 +2,6 @@ import { None as OpNone } from '../internal/types'
 import { None as Symbol } from '../internal/symbol'
 
 export const None = Object.freeze({
-  value: 0,
+  value: null,
   __type: Symbol,
 }) as OpNone
