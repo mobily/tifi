ad47ebb030fdad7de9b06553b8122639f4339a83
Marcin Dziewulski <hello@mobily.pl>
2019-03-12 10:21:13 +0100

    Update `fromNullable` test

--- src/option/fromNullable.test.ts
+++ src/option/fromNullable.test.ts
@@ -4,11 +4,16 @@ import { None } from './None'
 import { fromNullable } from './fromNullable'
 
 describe('fromNullable', () => {
-  test('None', () => {
+  it('is `None`', () => {
     expect(fromNullable(null)).toBe(None)
+    expect(fromNullable(undefined)).toBe(None)
   })
 
-  test('Some', () => {
-    expect(fromNullable(5)).toBe(Some(5))
+  it('is `Some`', () => {
+    expect(fromNullable('string')).toBe(Some('string'))
+    expect(fromNullable([0, 1, 2])).toEqual(Some([0, 1, 2]))
+    expect(fromNullable({ value: true })).toEqual(Some({ value: true }))
+    expect(fromNullable(0)).toBe(Some(0))
+    expect(fromNullable(false)).toBe(Some(false))
   })
 })
