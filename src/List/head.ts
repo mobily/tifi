91732446ef8b53e37516f0e088c05006abfcfc62
Marcin Dziewulski <hello@mobily.pl>
2019-03-14 22:59:18 +0100

    Tweaks

--- src/List/head.ts
+++ src/List/head.ts
@@ -5,6 +5,6 @@ import { Option, None as N, Some as S } from '../internal/types'
 export function head(list: [] | [null, ...any[]] | [undefined, ...any[]]): N
 export function head<T>(list: [T, ...any[]]): S<T>
 export function head<T>(list: T[]): Option<T> {
-  const [first] = list
-  return fromNullable(first)
+  const [hd] = list
+  return fromNullable(hd)
 }
