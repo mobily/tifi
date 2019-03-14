866cf4766385e42b8fbfd418732cb6b88a047d91
Marcin Dziewulski <hello@mobily.pl>
2019-03-14 22:59:10 +0100

    Update `tail` helper (List)

--- src/List/tail.test.ts
+++ src/List/tail.test.ts
@@ -6,15 +6,12 @@ import { Some } from '../Option/Some'
 describe('tail', () => {
   it('*', () => {
     expect(tail([])).toEqual(None)
-    expect(tail([1, 2, undefined])).toEqual(None)
-    expect(tail([1, 2, null])).toEqual(None)
   })
 
   it('*', () => {
-    expect(tail([1, 2, 3])).toEqual(Some(3))
-    expect(tail([1, 2, 0])).toEqual(Some(0))
-    expect(tail([true, true, false])).toEqual(Some(false))
-    expect(tail([{ prop: 1 }, { prop: 2 }])).toEqual(Some({ prop: 2 }))
-    expect(tail([[1], [2]])).toEqual(Some([2]))
+    expect(tail([1, 2, 3])).toEqual(Some([2, 3]))
+    expect(tail([true, true, false])).toEqual(Some([true, false]))
+    expect(tail([{ prop: 1 }, { prop: 2 }])).toEqual(Some([{ prop: 2 }]))
+    expect(tail([[1], [2], [3]])).toEqual(Some([[2], [3]]))
   })
 })
--- src/List/tail.ts
+++ src/List/tail.ts
@@ -1,8 +1,11 @@
-import { fromNullable } from '../Option/fromNullable'
+import { Some } from '../Option/Some'
+import { None } from '../Option/None'
 
-import { Option } from '../internal/types'
+import { Option, None as N, Some as S } from '../internal/types'
 
-export const tail = <T>(list: T[]): Option<T> => {
-  const [last] = list.slice(-1)
-  return fromNullable(last)
+export function tail(list: []): N
+export function tail<T>(list: [any, ...T[]]): S<T[]>
+export function tail<T>(list: T[]): Option<T[]> {
+  const [, ...tl] = list
+  return Boolean(tl.length) ? Some(tl) : None
 }
