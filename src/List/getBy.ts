497899325f26ecadc01a5de1077acac5f999284b
Marcin Dziewulski <hello@mobily.pl>
2019-03-16 16:01:51 +0100

    Simplify `getBy` implementation

--- src/List/getBy.ts
+++ src/List/getBy.ts
@@ -5,7 +5,5 @@ import { Option } from '../internal/types'
 export const getBy = <T>(
   predicate: (value: T, index: number) => boolean,
   list: T[],
-): Option<T> => {
-  const element = list.find((value, index) => predicate(value, index))
-  return fromNullable(element)
-}
+): Option<T> =>
+  fromNullable(list.find((value, index) => predicate(value, index)))
