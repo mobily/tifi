9819e9969594d132d5ffb3f1adc8da4d8faf183b
Marcin Dziewulski <hello@mobily.pl>
2019-03-16 10:29:50 +0100

    Update `drop` helper

--- src/List/drop.test.ts
+++ src/List/drop.test.ts
@@ -5,14 +5,17 @@ import { Some } from '../Option/Some'
 
 describe('drop', () => {
   it('*', () => {
-    expect(drop(0, [])).toEqual(None)
-    expect(drop(3, [1, 2, 3])).toEqual(None)
+    expect(drop(4, [1, 2, 3])).toEqual(None)
+    expect(drop(-1, [1, 2, 3])).toEqual(None)
   })
 
   it('*', () => {
+    expect(drop(0, [])).toEqual(Some([]))
+    expect(drop(0, [1, 2, 3])).toEqual(Some([1, 2, 3]))
+    expect(drop(1, [1])).toEqual(Some([]))
     expect(drop(1, [1, 2, 3])).toEqual(Some([2, 3]))
-    expect(drop(0, [0, 2, 3])).toEqual(Some([0, 2, 3]))
-    expect(drop(2, [true, true, false])).toEqual(Some([false]))
     expect(drop(1, [[1], [2]])).toEqual(Some([[2]]))
+    expect(drop(2, [true, true, false])).toEqual(Some([false]))
+    expect(drop(3, [1, 2, 3])).toEqual(Some([]))
   })
 })
--- src/List/drop.ts
+++ src/List/drop.ts
@@ -4,4 +4,4 @@ import { Some } from '../Option/Some'
 import { None } from '../Option/None'
 
 export const drop = <T>(index: number, list: T[]): Option<T[]> =>
-  index > list.length - 1 ? None : Some(list.slice(index, Infinity))
+  index >= 0 && index <= list.length ? Some(list.slice(index, Infinity)) : None
