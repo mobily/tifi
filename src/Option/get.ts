52006c7e52fa486d3233ee5c3ee9c10596ea4ba2
Marcin Dziewulski <hello@mobily.pl>
2019-03-16 22:29:38 +0100

    :sparkles: Add `get`

--- src/Option/get.ts
+++ src/Option/get.ts
@@ -1,1 +1,11 @@
-// TODO:
+import { isNone } from './isNone'
+
+import { Option } from '../internal/types'
+
+export const get = <T>(option: Option<T>): T | never => {
+  if (isNone(option)) {
+    throw new Error('None')
+  }
+
+  return option.value
+}
