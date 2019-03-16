10711ffdb0a8b894628a2a6093e88e36e9880508
Marcin Dziewulski <hello@mobily.pl>
2019-03-16 18:59:00 +0100

    Update `tail` types

--- src/List/tail.ts
+++ src/List/tail.ts
@@ -1,11 +1,9 @@
 import { Some } from '../Option/Some'
 import { None } from '../Option/None'
 
-import { Option, None as N, Some as S } from '../internal/types'
+import { Option } from '../internal/types'
 
-export function tail(list: []): N
-export function tail<T>(list: [any, ...T[]]): S<T[]>
-export function tail<T>(list: T[]): Option<T[]> {
+export const tail = <T>(list: T[]): Option<T[]> => {
   const [, ...tl] = list
   return Boolean(tl.length) ? Some(tl) : None
 }
