60aedf38e51d342cb58342943a6238562236a337
Marcin Dziewulski <hello@mobily.pl>
2019-03-17 10:05:52 +0100

    Rename `get` to `getExn`

--- src/Option/getExn.ts
+++ src/Option/getExn.ts
@@ -2,7 +2,7 @@ import { isNone } from './isNone'
 
 import { Option } from '../internal/types'
 
-export const get = <T>(option: Option<T>): T | never => {
+export const getExn = <T>(option: Option<T>): T | never => {
   if (isNone(option)) {
     throw new Error('None')
   }
