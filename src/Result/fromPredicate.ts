c7065e05f38b8fd7e514346d33f81ed4012c9b4d
Marcin Dziewulski <hello@mobily.pl>
2019-03-24 13:30:02 +0100

    Create `fromPredicate` (Result)

---
+++ src/Result/fromPredicate.ts
@@ -0,0 +1,11 @@
+import { fromNullable } from './fromNullable'
+import { Error } from './Error'
+
+import { Result } from '../internal/types'
+
+export const fromPredicate = <A, B>(
+  predicate: (value: A) => boolean,
+  error: B,
+  value: A,
+): Result<A, B> =>
+  predicate(value) ? fromNullable(value, error) : Error(error)
