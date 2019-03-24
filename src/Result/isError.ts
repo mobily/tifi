c849551573410eaae355a0caf7522166957bb097
Marcin Dziewulski <hello@mobily.pl>
2019-03-24 11:55:33 +0100

    Define type guards (isOk, isError)

--- src/Result/isError.ts
+++ src/Result/isError.ts
@@ -1,5 +1,5 @@
-import { Result } from '../internal/types'
+import { Result, Error as E } from '../internal/types'
 import { Error } from '../internal/symbol'
 
-export const isError = <A, B>(result: Result<A, B>): boolean =>
+export const isError = <A, B>(result: Result<A, B>): result is E<B> =>
   result.__type === Error
--- src/Result/isOk.ts
+++ src/Result/isOk.ts
@@ -1,5 +1,5 @@
-import { Result } from '../internal/types'
+import { Result, Ok as O } from '../internal/types'
 import { Ok } from '../internal/symbol'
 
-export const isOk = <A, B>(result: Result<A, B>): boolean =>
+export const isOk = <A, B>(result: Result<A, B>): result is O<A> =>
   result.__type === Ok
