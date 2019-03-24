a229611c5fd9787da619eb572ef65f1d389208b5
Marcin Dziewulski <hello@mobily.pl>
2019-03-24 13:29:36 +0100

    Update internal types

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -1,17 +1,12 @@
-interface OptionFn<T> {
+interface Fn<T> {
   value: NonNullable<T>
   __type: symbol
 }
 
-interface ResultFn<T> {
-  value: T
-  __type: symbol
-}
-
-export type Some<T> = OptionFn<T> & { _: 'Some' }
-export type None = OptionFn<any> & { _: 'None' }
-export type Ok<T> = ResultFn<T> & { _: 'Ok' }
-export type Error<T> = ResultFn<T> & { _: 'Error' }
+export type Some<T> = Fn<T> & { _: 'Some' }
+export type None = Fn<any> & { _: 'None' }
+export type Ok<T> = Fn<T> & { _: 'Ok' }
+export type Error<T> = Fn<T> & { _: 'Error' }
 
 export type Option<T> = None | Some<T>
 export type Result<A, B> = Ok<A> | Error<B>
