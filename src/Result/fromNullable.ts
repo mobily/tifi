9c4d56dc71d99133ce603fa4fb6a40cceef6d083
Marcin Dziewulski <hello@mobily.pl>
2019-03-24 16:28:47 +0100

    Remove redundant overloads (fromNullable, Result)

--- src/Result/fromNullable.ts
+++ src/Result/fromNullable.ts
@@ -1,14 +1,12 @@
 import { Ok } from './Ok'
 import { Error } from './Error'
 
-import { Result, Error as E, Ok as O } from '../internal/types'
+import { Result } from '../internal/types'
 
-export function fromNullable<B>(error: B, value: null | undefined): E<B>
-export function fromNullable<A, B>(error: B, value: A): O<A>
-export function fromNullable<A, B>(
+export const fromNullable = <A, B>(
   error: B,
   value: A | null | undefined,
-): Result<A, B> {
+): Result<A, B> => {
   return value === null || typeof value === 'undefined'
     ? Error(error)
     : Ok(value)
