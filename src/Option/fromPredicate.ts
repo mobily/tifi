6fd4fa466159a804921191d803dd62684f1a3398
Marcin Dziewulski <hello@mobily.pl>
2019-03-24 13:29:10 +0100

    Update `fromPredicate` helper (Option)

--- src/Option/fromPredicate.ts
+++ src/Option/fromPredicate.ts
@@ -1,9 +1,9 @@
-import { Some } from './Some'
 import { None } from './None'
+import { fromNullable } from './fromNullable'
 
 import { Option } from '../internal/types'
 
 export const fromPredicate = <T>(
   predicate: (value: T) => boolean,
   value: T,
-): Option<T> => (predicate(value) ? Some(value) : None)
+): Option<T> => (predicate(value) ? fromNullable(value) : None)
